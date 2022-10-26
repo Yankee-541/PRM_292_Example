13a0b5b2c6ad2f4450dfa6fdc6febfdc3a65094c
D U Y Đ Ă N G <dangtd5401@gmail.com>
2022-10-26 17:43:09 +0700

    test add question

--- app/src/main/java/com/example/firstapp/AddActivity.java
+++ app/src/main/java/com/example/firstapp/AddActivity.java
@@ -2,23 +2,28 @@ package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class AddActivity extends AppCompatActivity {
 
-    EditText title, author, pages;
-    Button add_button;
+    EditText title, author, pages, size;
+    Button add_button, add_pic;
+    Context context;
+    LinearLayout list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-
-        title = findViewById(R.id.title_input);
-        author = findViewById(R.id.author_input);
+        context = this;
+//        title = findViewById(R.id.title_input);
+//        author = findViewById(R.id.author_input);
         pages = findViewById(R.id.pages_input);
         add_button = findViewById(R.id.add_button);
         add_button.setOnClickListener(new View.OnClickListener() {
@@ -33,4 +38,22 @@ public class AddActivity extends AppCompatActivity {
 
 
     }
+
+    private void addView(){
+        final View aa = getLayoutInflater().inflate(R.layout.add_cam, null, false);
+        ImageView cross =(ImageView) aa.findViewById(R.id.cross2);
+        cross.setVisibility(View.VISIBLE);
+        cross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeView(aa);
+            }
+        });
+        list.addView(aa);
+    }
+
+    private void removeView(View view){
+        list.removeView(view);
+    }
+
 }
---
+++ app/src/main/java/com/example/firstapp/add_cam.kt
@@ -0,0 +1,11 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_cam : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_cam)
+    }
+}
